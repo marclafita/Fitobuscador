4308b08b89b0b58487d2cbd447c590552f3d7fee
Marc Lafita <marclafita@gmail.com>
2016-11-01 02:37:33 +0100

    first commit!

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fitobuscadorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fitobuscadorApp
+ */
+angular.module('fitobuscadorApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
